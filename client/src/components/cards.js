c7885b8635f036c711dce5c2cf5f60da41be849d
Abidijihed <abidij55@gmail.com>
2021-04-16 13:21:46 +0200

    save

--- client/src/components/cards.js
+++ client/src/components/cards.js
@@ -1,26 +1,54 @@
 import * as React from 'react';
 import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
 import { StyleSheet, Text, View } from 'react-native';
-const MyComponent = () => (
-    <View >
-  <Card style={styles.container}>
-    <Card.Cover source={{ uri: 'https://d1fmx1rbmqrxrr.cloudfront.net/cnet/optim/i/edit/2019/04/eso1644bsmall__w770.jpg' }} />
-     <Card.Content>
-      <Title>Card title</Title>
-      <Paragraph>Card content</Paragraph>
-    </Card.Content>
-    <Card.Actions>
-      <Button>Cancel</Button>
-      <Button>Ok</Button>
-    </Card.Actions>
-  </Card>
-  </View>
-);
+import axios from "react-native-axios"
+
+
+export default class Cards extends React.Component {
+constructor(props){
+  super(props)
+  this.state={
+    data:[]
+  }
+}
+  componentDidMount(){
+    axios.get("http://localhost:3333/api/poducts")
+    .then((res)=>{
+      this.setState({data:res.data})
+    }).catch((err)=>{
+      console.log(err)
+    })
+  }
+  render() {
+    return (
+      <View >
+        {this.state.data.map((e)=>{
+          return (
+            <View>
+                   <Card style={styles.container}>
+      <Card.Cover source={{ uri:e.image }} />
+       <Card.Content>
+        <Title>{e.name}</Title>
+        <Paragraph>{e.information}</Paragraph>
+      </Card.Content>
+      <Card.Actions>
+        <Button>{e.status}</Button>
+        <Button>{e.oldprice}TND</Button>
+      </Card.Actions>
+    </Card>
+            </View>
+          )
+        })}
+   
+    </View>
+  );
+
+  }
+}
 const styles = StyleSheet.create({
-    container: {
-      marginTop: 0,
-      
+  container: {
+    marginTop: 0,
     
-    }
-  });
-export default MyComponent;
+  
+  }
+});
--- client/src/scenes/product.js
+++ client/src/scenes/product.js
@@ -1,22 +1,56 @@
-import React from 'react'
-import { Button, StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
+import { StyleSheet, Text, View } from 'react-native';
+import axios from "react-native-axios"
+import Constants from 'expo-constants';
 
 
-const product = () => {
-//     const pressHandler =()=> {
-//         navigation.goBack();
-//     }
-    return (
-        <View>
-            <Text>hello product</Text>
-            <Text>hello product</Text>
-            <Text>hello product</Text>
-            <Text>hello product</Text>
-            <Button title='go back to home page' />
-
-        </View>
-    )
+export default class product extends React.Component {
+constructor(props){
+  super(props)
+  this.state={
+    data:[]
+  }
 }
 
-export default product
+  componentDidMount(){
+    axios.get("http://localhost:3333/api/poducts")
+    .then((res)=>{
+      this.setState({data:res.data})
+    }).catch((err)=>{
+      console.log(err)
+    })
+  }
+  render() {
+    return (
+      <View style={{flex:1}}>
+        {this.state.data.map((e)=>{
+          return (
+            <View key={e.id}>
+                   <Card style={styles.container}>
+      <Card.Cover source={{ uri:e.image }} />
+       <Card.Content>
+        <Title>{e.name}</Title>
+        <Paragraph>{e.information}</Paragraph>
+      </Card.Content>
+      <Card.Actions>
+        <Button>{e.status}</Button>
+        <Button>{e.oldprice}TND</Button>
+      </Card.Actions>
+    </Card>
+            </View>
+          
+        )})}
+   
+    </View>
+  );
 
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 0,
+    
+  
+  }
+});
