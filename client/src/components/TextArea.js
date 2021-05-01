190e038083e1126e0ad57ced57bab18b7bed773c
rmadi <rmadi.med1@gmail.com>
2021-05-01 03:08:41 +0200

    fic some style for contact component

---
+++ client/src/components/TextArea.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import {View, TextInput, StyleSheet} from 'react-native';
+import {windowHeight, windowWidth} from '../utils/Dimentions';
+
+import AntDesign from 'react-native-vector-icons/AntDesign';
+
+const TextArea = ({labelValue, placeholderText, iconType, ...rest}) => {
+    return (
+        <View style={styles.inputContainer}>
+     
+        <TextInput
+          value={labelValue}
+          style={styles.input}
+        
+          numberOfLines={10}
+          multiline={true}
+          placeholder={placeholderText}
+          placeholderTextColor="#666"
+          {...rest}
+        />
+      </View>
+    )
+}
+
+export default TextArea
+const styles = StyleSheet.create({
+    inputContainer: {
+      marginTop: 5,
+      marginBottom: 10,
+      width: '100%',
+      height:'50%',
+      borderColor: '#ccc',
+      borderRadius: 3,
+      borderWidth: 1,
+      flexDirection: 'row',
+      alignItems: 'center',
+      backgroundColor: '#fff',
+    },
+    iconStyle: {
+      padding: 10,
+      height: '100%',
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderRightColor: '#ccc',
+      borderRightWidth: 1,
+      width: 50,
+    },
+    input: {
+      padding: 10,
+      flex: 1,
+      fontSize: 16,
+      fontFamily: 'Lato-Regular',
+      color: '#333',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height:300
+    },
+    inputField: {
+      padding: 10,
+      marginTop: 5,
+      marginBottom: 10,
+      width: windowWidth / 1.5,
+      height: windowHeight / 15,
+      fontSize: 16,
+      borderRadius: 8,
+      borderWidth: 1,
+    },
+  });
--- client/src/scenes/Contact.js
+++ client/src/scenes/Contact.js
@@ -1,24 +1,27 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, View } from 'react-native'
-import { TextInput } from 'react-native-gesture-handler';
+import { ScrollView, TextInput } from 'react-native-gesture-handler';
 import FromInput from "../components/FormInput";
 import { Button } from '@ui-kitten/components';
 import axios from "react-native-axios";
+import TextArea from '../components/TextArea';
 
 
 
 const Contact = ({navigaton}) => {
     const [email, setEmail] = useState();
     const [text, setText] = useState();
-    console.log('text',text)
-    console.log('email', email)
+
     const onSendEMail = ()=> {
       axios.post('http://192.168.1.15:3333/sendmail',{email, text})
-      .then((res)=>{navigation.navigate('Home')})
+      .then((res)=>{console.log(res); navigaton.navigate('HOME')})
       .catch((e)=>{console.log(e)})
     }
 
     return (
+      <ScrollView>
+
+      
         <View style={styles.conntainer}>
 
         <FromInput
@@ -30,8 +33,18 @@ const Contact = ({navigaton}) => {
         autoCapitalize="none"
         autoCorrect={false}
       />
+            <TextArea
+            labelValue={text}
+            onChangeText={(text) => setText(text)}
+             underlineColorAndroid="transparent"
+             placeholder="Type something"
+             placeholderTextColor="grey"
+             numberOfLines={10}
+             multiline={true}
+       
+         />
       
-        <TextInput
+        {/* <TextInput
         style={styles.input}
          labelValue={text}
          onChangeText={(text) => setText(text)}
@@ -40,15 +53,16 @@ const Contact = ({navigaton}) => {
           placeholderTextColor="grey"
           numberOfLines={10}
           multiline={true}
-        />
+        /> */}
         <Button
          style={styles.btn}
          onPress={()=>onSendEMail()}
         
         >Submit</Button>
-       
+  
         
       </View>
+      </ScrollView>
     )
 }
 
