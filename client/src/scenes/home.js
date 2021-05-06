4db2e0b9badd61e6a50fe69bb1ce9de193d7912d
rmadi <rmadi.med1@gmail.com>
2021-05-06 18:19:55 +0200

    first screen in payment style

--- client/src/scenes/home.js
+++ client/src/scenes/home.js
@@ -2,15 +2,26 @@ import React, { useState, useRef, useEffect } from "react";
 import axios from "react-native-axios";
 import {
   StyleSheet,
-  Text,
   View,
+  Text
 
 } from "react-native";
+import { Button, Icon, Layout, Spinner } from '@ui-kitten/components';
+
+
 
 const home = () => {
 
-
-
+  const StarIcon = (props) => (
+    <Icon {...props} name='star'/>
+  );
+  
+  const LoadingIndicator = (props) => (
+    <View style={[props.style, styles.indicator]}>
+      <Spinner size='small'/>
+    </View>
+  );
+  
  
 
 
@@ -18,8 +29,41 @@ const home = () => {
     <View style={{ flex: 1, backgroundColor: "#1d5aa9", marginRight: 20 }}>
      
       <Text>hello Home Page</Text>
+
+      <Button status='success' style={styles.button}>
+      SUCCESS
+         </Button>
+
+         {/* accessoryLeft={LoadingIndicator} */}
+    <Button style={styles.load} appearance='outline' accessoryLeft={LoadingIndicator}>
+      ..LOADING
+    </Button>
+
+
     </View>
   );
 };
 
 export default home;
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  button: {
+    margin: 20,
+    // width:100,
+    // height:100
+  },
+  load: {
+    margin:30,
+    backgroundColor:'#3c40c6'
+  },
+  indicator: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
